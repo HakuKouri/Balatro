7eff515defaa67dc76f27e8e80d8dbe5cab3ceda
HakuKouri <taibi89@gmx.net>
2024-11-28 15:45:59 +0100

    TODO WebHandler

--- src/main/java/com/example/balatro/classes/WebHandler.java
+++ src/main/java/com/example/balatro/classes/WebHandler.java
@@ -14,10 +14,19 @@ public class WebHandler
         {
             Document doc = Jsoup.connect("https://balatrogame.fandom.com/wiki/Jokers").get();
             System.out.println(doc.title());
-            Elements tables = doc.select("table").;
-            for (Element table : tables) {
-                System.out.println(String.format("%s\n", table.attr("class")));
+            Elements tables = doc.select("table");
+            Element table = tables.get(1);
+            Elements rows = table.select("tr");
+            for (Element row : rows)
+            {
+                Elements cells = row.select("td");
+                for (Element cell : cells )
+                {
+                    System.out.println(cell);
+                }
+
             }
+
         } catch (IOException e)
         {
             throw new RuntimeException(e);
